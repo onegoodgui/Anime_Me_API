a1a49f755d8cb24e1344259b8e77e363e231c1c8
Guilherme Dallagnol Vargas <gui_dvargas@hotmail.com>
2022-07-24 20:38:30 -0300

    fix: anime schema romaji title

--- src/schemas/animeSchema.ts
+++ src/schemas/animeSchema.ts
@@ -1,16 +1,19 @@
-import joi from 'joi'
+import joi from "joi";
 
 export const animeDataSchema = joi.object({
-    id: joi.number().required(),
-    title: joi.object({
-        english: joi.string().required()
-    }),
-    averageScore: joi.number().required(),
-    status: joi.string().valid("watched", "watching", "to_watch").required(),
-    description: joi.string(),
-    genres: joi.array().items(joi.string()).required(),
-    coverImage: joi.object({
-        large: joi.string(),
-        medium: joi.string()
+  id: joi.number().required(),
+  title: joi
+    .object({
+      english: joi.string().allow(null),
+      romaji: joi.string().allow(null),
     })
-})
+    .required(),
+  averageScore: joi.number().required(),
+  status: joi.string().valid("watched", "watching", "to_watch").required(),
+  description: joi.string(),
+  genres: joi.array().items(joi.string()).required(),
+  coverImage: joi.object({
+    large: joi.string(),
+    medium: joi.string(),
+  }),
+});
